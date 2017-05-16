dde04331f91eed994d6b4d7f449d03f6ae45988b
Anton Orzes <antonorzes@antons-mac-mini.local>
2017-05-16 18:01:57 +0200

    Initial Commit

---
+++ PickerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PickerTest
+//
+//  Created by Anton Orzes on 16/05/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PickerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PickerTest
+//
+//  Created by Anton Orzes on 16/05/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
